7aaac78da26bcd21e3e194d177f50e25b66b6559
denis <denisakov@gmail.com>
2013-02-26 21:03:55 +0200

    Just pulled the app to new computer

---
+++ README.md
@@ -0,0 +1,5 @@
+# CarbonBase: first application of the whole system - De-bundling checker
+
+This is the first application for
+[*Carbonbase*](http://carbonbase.org/)
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carbonbase::Application.config.secret_token = 'a9272e205adb399c11ce29b45b652142f6c5e55506f37562f00c3474b69405ba0767843185c805bbc7f8406153415cd061eae608f9d44fcea0476afc47b5e185'
